1b19e04dd1b682f0266b9c13bddc80ea03b4ecb6
Gyselle Marques <gmsilva619@gmail.com>
2024-07-27 19:44:52 -0300

    desafio finalizado com spring framework

---
+++ README.md
@@ -0,0 +1,16 @@
+# Desafio DIO: Design Patterns com Spring Framework
+
+Desafio realizado com o ojetivo de aplicar o conceito de Padrões de Projeto na prática, implementando soluções que utilizem os padrões clássicos do GoF e as abstrações fornecidas pelo Spring Framework.
+
+Padrões utilizados:
+
+- Singleton
+- Strategy/Repository
+- Facade
+
+##
+
+<h3>💻 Tecnologias</h3>
+
+![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)
+![Spring](https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white)
---
+++ src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * Foram selecionados os seguintes módulos:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ * 
+ * @autor gyselle-marques
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
